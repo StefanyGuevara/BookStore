a485c50e26d765334ec2eb4e702b03ee90b4f64b
StefanyGuevara <stefany.m.guevara00@gmail.com>
2022-03-16 00:20:26 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using BookStore.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookStoreRepository repo;
+
+        //private BookstoreContext context { get; set; }
+
+        public HomeController(IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(string bookType, int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .Where(b => b.Category == bookType || bookType == null)
+                .OrderBy(b => b.BookId)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks =
+                        (bookType == null
+                            ? repo.Books.Count()
+                            : repo.Books.Where(x => x.Category == bookType).Count()),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+            return View(x);
+        }
+    }
+}
+
+
+//public HomeController(BookstoreContext temp)
+//{
+//    context = temp;
+//}
+// using => lamba symbol is a short cut to the below commented out section 
