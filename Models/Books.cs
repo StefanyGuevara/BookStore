90d7ab8a400bc18f0f6b161daad11f2adafa0af3
StefanyGuevara <stefany.m.guevara00@gmail.com>
2022-03-19 08:26:31 -0600

    Final Changes to mission 10

--- Models/Books.cs
+++ Models/Books.cs
@@ -13,6 +13,8 @@ namespace BookStore.Models
         [Key]
         [Required]
         public int BookId { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
